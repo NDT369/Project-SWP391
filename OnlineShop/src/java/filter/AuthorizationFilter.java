833e5bbc6499ec2e95fec0d3e46f81419dcd0ba2
NDT369 <92255832+NDT369@users.noreply.github.com>
2022-05-31 00:10:48 +0700

    Update AuthorizationFilter.java

--- OnlineShop/src/java/filter/AuthorizationFilter.java
+++ OnlineShop/src/java/filter/AuthorizationFilter.java
@@ -25,17 +25,17 @@ import model.Account;
  * @author DUC THINH
  */
 public class AuthorizationFilter implements Filter {
-    
+
     private static final boolean debug = true;
 
     // The filter configuration object we are associated with.  If
     // this value is null, this filter instance is not currently
     // configured. 
     private FilterConfig filterConfig = null;
-    
+
     public AuthorizationFilter() {
-    }    
-    
+    }
+
     private void doBeforeProcessing(ServletRequest request, ServletResponse response)
             throws IOException, ServletException {
         if (debug) {
@@ -62,8 +62,8 @@ public class AuthorizationFilter implements Filter {
 	    log(buf.toString());
 	}
          */
-    }    
-    
+    }
+
     private void doAfterProcessing(ServletRequest request, ServletResponse response)
             throws IOException, ServletException {
         if (debug) {
@@ -101,20 +101,19 @@ public class AuthorizationFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response,
             FilterChain chain)
             throws IOException, ServletException {
-        
+
         if (debug) {
             log("AuthorizationFilter:doFilter()");
         }
-        
+
         doBeforeProcessing(request, response);
-      HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
         String url = httpRequest.getServletPath();
         HttpSession session = httpRequest.getSession();
         Account a = (Account) session.getAttribute("account");
         switch (url) {
-            case "/admindashboard": 
-            {
+            case "/admindashboard": {
                 if (a == null) {
                     httpResponse.sendRedirect("login");
                 } else {
@@ -126,9 +125,8 @@ public class AuthorizationFilter implements Filter {
                 }
                 break;
             }
-            
-            case "/saledashboard":
-            {
+
+            case "/saledashboard": {
                 if (a == null) {
                     httpResponse.sendRedirect("login");
                 } else {
@@ -140,9 +138,8 @@ public class AuthorizationFilter implements Filter {
                 }
                 break;
             }
-            
-            case "/marketingdashboard":
-            {
+
+            case "/marketingdashboard": {
                 if (a == null) {
                     httpResponse.sendRedirect("login");
                 } else {
@@ -154,10 +151,9 @@ public class AuthorizationFilter implements Filter {
                 }
                 break;
             }
-   
+
         }
 
-        
         Throwable problem = null;
         try {
             chain.doFilter(request, response);
@@ -168,7 +164,7 @@ public class AuthorizationFilter implements Filter {
             problem = t;
             t.printStackTrace();
         }
-        
+
         doAfterProcessing(request, response);
 
         // If there was a problem, we want to rethrow it if it is
@@ -203,16 +199,16 @@ public class AuthorizationFilter implements Filter {
     /**
      * Destroy method for this filter
      */
-    public void destroy() {        
+    public void destroy() {
     }
 
     /**
      * Init method for this filter
      */
-    public void init(FilterConfig filterConfig) {        
+    public void init(FilterConfig filterConfig) {
         this.filterConfig = filterConfig;
         if (filterConfig != null) {
-            if (debug) {                
+            if (debug) {
                 log("AuthorizationFilter:Initializing filter");
             }
         }
@@ -231,20 +227,20 @@ public class AuthorizationFilter implements Filter {
         sb.append(")");
         return (sb.toString());
     }
-    
+
     private void sendProcessingError(Throwable t, ServletResponse response) {
-        String stackTrace = getStackTrace(t);        
-        
+        String stackTrace = getStackTrace(t);
+
         if (stackTrace != null && !stackTrace.equals("")) {
             try {
                 response.setContentType("text/html");
                 PrintStream ps = new PrintStream(response.getOutputStream());
-                PrintWriter pw = new PrintWriter(ps);                
+                PrintWriter pw = new PrintWriter(ps);
                 pw.print("<html>\n<head>\n<title>Error</title>\n</head>\n<body>\n"); //NOI18N
 
                 // PENDING! Localize this for next official release
-                pw.print("<h1>The resource did not process correctly</h1>\n<pre>\n");                
-                pw.print(stackTrace);                
+                pw.print("<h1>The resource did not process correctly</h1>\n<pre>\n");
+                pw.print(stackTrace);
                 pw.print("</pre></body>\n</html>"); //NOI18N
                 pw.close();
                 ps.close();
@@ -261,7 +257,7 @@ public class AuthorizationFilter implements Filter {
             }
         }
     }
-    
+
     public static String getStackTrace(Throwable t) {
         String stackTrace = null;
         try {
@@ -275,9 +271,9 @@ public class AuthorizationFilter implements Filter {
         }
         return stackTrace;
     }
-    
+
     public void log(String msg) {
-        filterConfig.getServletContext().log(msg);        
+        filterConfig.getServletContext().log(msg);
     }
-    
+
 }
